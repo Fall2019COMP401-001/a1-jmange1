7a38e04ed25fa5d5d73d50e147f01922e59912ba
730226346 <730226346@730226346-pc.wireless-1x.unc.edu>
2019-09-01 19:52:42 -0400

    Jedi done.

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -10,5 +10,57 @@ public class A1Jedi {
 
 		// Your code follows here.
 		
+		int count = scan.nextInt();
+		
+		String[] options = new String[count];
+		
+		double[] prices = new double[count];
+		int[] purchased = new int[count];
+		
+		for (int i = 0; i < count; i++) {
+			options[i] = scan.next();
+			prices[i] = scan.nextDouble();
+		}
+		int customers = scan.nextInt();
+		
+		String[] names = new String[customers];
+		
+		double[] totals = new double[customers];
+		
+		int[] whoBought = new int[count];
+		
+		boolean[] alreadyBought = new boolean[count];
+		
+		for (int i = 0; i < customers; i++) {
+			names[i] = scan.next() + " " + scan.next();
+			int items = scan.nextInt();
+			for (int w = 0; w < count; w++) {
+				alreadyBought[w] = false;
+			}
+			for (int j = 0; j < items; j++) {
+				int howMany = scan.nextInt();
+				String item = scan.next();
+				for (int q = 0; q < options.length; q++) {
+					if (item.equals(options[q])) {
+						purchased[q] += howMany;
+						if (!alreadyBought[q]) {
+						whoBought[q]++;
+						alreadyBought[q] = true;
+						}
+					}
+				}
+			}
+		}
+		for (int i = 0; i < count; i++) {
+			if (whoBought[i] == 0) {
+				System.out.println("No customers bought " 
+				+ options[i]);
+			} else {
+				System.out.println(whoBought[i] + 
+				" customers bought " + purchased[i] +
+				" " + options[i]);
+			}
+		}
+		
 	}
 }
