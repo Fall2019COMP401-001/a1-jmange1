3dd682a867b33b15814fa753c52317da4cb87348
sgo21 <54336205+sgo21@users.noreply.github.com>
2019-09-02 03:00:52 -0400

    took away [][] way reworked the old way
    still cant figure out why committing and pushing wont work so had to copy and paste from eclipse to here

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -1,66 +1,83 @@
 package a1;
 
+
 import java.util.Scanner;
 
+
 public class A1Jedi {
-
+	
 	public static void main(String[] args) {
-		
-		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
-		
-		int count = scan.nextInt();
-		
-		String[] options = new String[count];
-		
-		double[] prices = new double[count];
-		int[] purchased = new int[count];
-		
-		for (int i = 0; i < count; i++) {
-			options[i] = scan.next();
-			prices[i] = scan.nextDouble();
-		}
-		int customers = scan.nextInt();
-		
-		String[] names = new String[customers];
-		
-		double[] totals = new double[customers];
-		
-		int[] whoBought = new int[count];
-		
-		boolean[] alreadyBought = new boolean[count];
-		
-		for (int i = 0; i < customers; i++) {
-			names[i] = scan.next() + " " + scan.next();
-			int items = scan.nextInt();
-			for (int w = 0; w < count; w++) {
-				alreadyBought[w] = false;
+			
+			Scanner scan = new Scanner(System.in);
+	 
+			
+			int count  = scan.nextInt();
+			
+			// customers amounts, items, and price arrays
+			// changed amountItems[] to a [][] array
+			//ignore price
+			int [] amountItems = new int[count]; 
+			String [] itemName = new String [count];
+			double[] price = new double [count];
+						
+			//putting all items in an array; ignore price array input
+			for (int i=0; i<count; i++) {
+				itemName [i] = scan.next(); 
+				price [i]= scan.nextDouble(); 
 			}
-			for (int j = 0; j < items; j++) {
-				int howMany = scan.nextInt();
-				String item = scan.next();
-				for (int q = 0; q < options.length; q++) {
-					if (item.equals(options[q])) {
-						purchased[q] += howMany;
-						if (!alreadyBought[q]) {
-						whoBought[q]++;
-						alreadyBought[q] = true;
-						}
+		
+			// total customers; ignore priceArray and custName arays
+			
+			int customers = scan.nextInt(); 
+			
+			int [] custAmount =  new int[count]; 
+			String[] custName = new String[customers];
+			double [] priceArray =  new double[customers]; 
+			boolean[] duplicates = new boolean[count]; 
+			// going through each customer; ignore customer names for jedi
+
+			for (int j=0; j<customers; j++) {
+				//feed in names ignore for jedi
+				String firstName =  scan.next();
+				String lastName = scan.next();
+				
+				int totalqty = scan.nextInt();
+				
+				//check for duplicates;  account for them later
+				for (int t=0; t<count; t++) {
+					duplicates[t] = false; 
+				}
+				
+				for (int n=0; n<totalqty; n++) {
+					int eachqty = scan.nextInt();
+					String purchasedItem = scan.next();
+				
+					
+					for (int p=0; p<itemName.length; p++) {
+						if (purchasedItem.equals(itemName[p])) {
+							amountItems[p]+= eachqty;
+							// account for duplicates
+							if (!duplicates[p]) {
+								custAmount[p]++; 
+								duplicates[p] = true; 
+							}
+						}				
 					}
+				
+					}
+				
+			}
+			
+			
+			for (int i=0; i<count; i++) {
+				if (custAmount[i] == 0) {
+					System.out.println("No customers bought " + itemName[i]); ;
+				} else {
+					System.out.println(custAmount[i] + " customers bought " + amountItems[i] + " " + itemName[i]); 
 				}
 			}
+			scan.close(); 
+			
 		}
-		for (int i = 0; i < count; i++) {
-			if (whoBought[i] == 0) {
-				System.out.println("No customers bought " 
-				+ options[i]);
-			} else {
-				System.out.println(whoBought[i] + 
-				" customers bought " + purchased[i] +
-				" " + options[i]);
-			}
-		}
-		
-	}
+
 }
